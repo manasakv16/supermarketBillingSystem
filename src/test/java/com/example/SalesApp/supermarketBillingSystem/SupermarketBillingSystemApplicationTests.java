828bdd44c1615981acf3b28d8d2112642922fb91
manasakv16 <manasakv16@gmail.com>
2023-10-09 20:44:07 +0530

    Initial commit

---
+++ src/test/java/com/example/SalesApp/supermarketBillingSystem/SupermarketBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SalesApp.supermarketBillingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
